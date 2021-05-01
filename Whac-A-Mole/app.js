fa6e802cfc62fa0ee505302bae88cd4760842ba6
Elli Raynai <elli@cinehackers.com>
2021-05-01 09:29:42 -0400

    Number guessing game added

--- Whac-A-Mole/app.js
+++ Whac-A-Mole/app.js
@@ -1,3 +1,10 @@
 const squares = document.querySelectorAll(".square");
 const mole = document.querySelector(".mole");
 const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
+
+let result = 0;
+
+function randomSquare() {
+  squares.forEach()
+}
